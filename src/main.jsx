69a0cd99d6c495ddb15bd454632f5ccd23c2ca44
Sudeep Mukul <sudeepmukul@gmail.com>
2026-09-14 16:25:49 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary';
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <HelmetProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </HelmetProvider>
     </ErrorBoundary>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ieboard/',
   plugins: [react()],
   server: {
     // Force dependency optimization
